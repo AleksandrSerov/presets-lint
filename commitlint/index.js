492e7b4dacfa19a4cee7569ab635f4f3cc31b860
Aleksandr Serov <sashaserov2010@gmail.com>
2022-04-23 14:32:19 +0300

    chore(*):

--- commitlint/index.js
+++ commitlint/index.js
@@ -1,1 +1,6 @@
-module.exports = {};
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'scope-empty': [2, 'never'],
+    },
+};
--- prettier/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  printWidth: 100,
-  singleQuote: true,
-  jsxSingleQuote: true,
-  tabWidth: 4,
-  useTabs: true,
-  trailingComma: "all",
-};
